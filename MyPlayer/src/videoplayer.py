b95af30043a7a29895a7d63391e8313bc065a53f
Jesse Zhang <d@users.noreply.github.com>
2012-10-22 15:02:58 +0000

    Skeleton for Streaming and Youtube search

--- MyPlayer/src/videoplayer.py
+++ MyPlayer/src/videoplayer.py
@@ -12,6 +12,7 @@ import os
 import random
 from PyQt4 import QtCore, QtGui, uic, phonon
 
+# Two additional UI module.
 import addurl
 import about
 
@@ -25,15 +26,16 @@ class VideoPlayer(QtGui.QMainWindow):
         # Initialize: calling the inherited __init__ function.
         #######################################################################
         super(VideoPlayer, self).__init__()
-        
+
 
         #######################################################################
         # Load the defined UI.
         #######################################################################
-        uic.loadUi('../share/ui/videoplayer_mod.ui', self)
+        uic.loadUi('../share/ui/videoplayer_mod_add_search.ui', self)
 
         #######################################################################
         # Initialize the play list.
+        # Set some initial playing option.
         #######################################################################
         self.playlist = []
         self.playlistTmp = []
@@ -58,25 +60,38 @@ class VideoPlayer(QtGui.QMainWindow):
         #Connect these to actions.
         self.axnAddLocalFile.triggered.connect(self.on_axnAddLocalFile_triggered)
         self.axnAddURL.triggered.connect(self.on_axnAddURL_triggered)
-        
+
         #######################################################################
         # Create a timer. This will be used to show the playing time.
         #######################################################################
         self.tmrTimer = QtCore.QTimer(self)
         # This will emit a signal every 1/4 second.
-        self.tmrTimer.setInterval(250)        
+        self.tmrTimer.setInterval(250)
         # Connect the signal emitted to the action self.on_tmrTimer_timeout
-        self.tmrTimer.timeout.connect(self.on_tmrTimer_timeout)        
+        self.tmrTimer.timeout.connect(self.on_tmrTimer_timeout)
         # start the timer.
-        self.tmrTimer.start() 
-        
+        self.tmrTimer.start()
+
         #######################################################################
         # initialize the current mouse position and time.
         #######################################################################
         self.mousePos0 = QtGui.QCursor.pos()
         self.mouseT0 = QtCore.QTime.currentTime()
+
         
+        #######################################################################
+        # The search tab.
+        #######################################################################
+        # Search box: Enter pressed.
+        self.lineEditSearch.returnPressed.connect(self.on_btnSearchVideo_clicked)
         
+    
+    #Click on the 'search' button in the search tab.
+    @QtCore.pyqtSlot()
+    def on_btnSearchVideo_clicked(self):
+        print "Search button pressed"
+        print str(self.lineEditSearch.text())
+        self.lineEditSearch.selectAll()
         
     # Event: The 'About' button is clicked
     @QtCore.pyqtSlot()
@@ -90,7 +105,7 @@ class VideoPlayer(QtGui.QMainWindow):
         # Clear the original and temporal play list.
         self.playlist = []
         self.playlistTmp = []
-     
+
         self.updatePlayList()
 
     # Event: The 'Full screen' button is clicked.
@@ -109,7 +124,7 @@ class VideoPlayer(QtGui.QMainWindow):
             self.dckPlayList.hide()
             # Hide the mouse cursor.
             self.setCursor(QtGui.QCursor(QtCore.Qt.BlankCursor))
-            
+
     # Event: The next button is pressed.
     @QtCore.pyqtSlot()
     def on_btnNext_clicked(self):
@@ -118,7 +133,7 @@ class VideoPlayer(QtGui.QMainWindow):
             return
         # Get the reference to the current selected media.
         curIndex = self.lswPlaylist.currentRow()
-        
+
         if curIndex + 1 < len(self.playlist):
             # Not the last video
             index = curIndex + 1
@@ -129,47 +144,49 @@ class VideoPlayer(QtGui.QMainWindow):
                 return
             else:
                 index = 0
-            
+
         # Select the next media in the play list, and play it.
         self.lswPlaylist.setCurrentRow(index, QtGui.QItemSelectionModel.SelectCurrent)
         self.on_lswPlaylist_doubleClicked(self.lswPlaylist.item(index))
-        
-            
-                
-        
+
+
+
+
 
     # Event: The 'play/pause' is pressed.
     @QtCore.pyqtSlot()
     def on_btnPlayPause_clicked(self):
-        # If there is some selected item, and it is either 'playing' or 
+        # If there is some selected item, and it is either 'playing' or
         # 'paused', then switch between the two states.
         if self.vdpVideo.mediaObject().state() == phonon.Phonon.PlayingState:
+            print "Change to pause"
             self.vdpVideo.pause()
-        elif self.vdpVideo.mediaObject().state() == phonon.Phonon.PausedState:
+        else: # self.vdpVideo.mediaObject().state() == phonon.Phonon.PausedState:
+            print "Change to play"
             self.vdpVideo.play()
-            
-        # If there aren't a selected media in the play list, then select the 
+
+        # If there aren't a selected media in the play list, then select the
         # first one in the list and play it.
-        elif self.lswPlaylist.currentRow() < 0:
+        #elif self.lswPlaylist.currentRow() < 0:
             # Select the first item.
-            self.lswPlaylist.setCurrentRow(0, QtGui.QItemSelectionModel.\
-                                                    SelectCurrent)
+            #self.lswPlaylist.setCurrentRow(0, QtGui.QItemSelectionModel.\
+            #                                        SelectCurrent)
             # Call this to play it.
-            self.on_lswPlaylist_doubleClicked(self.lswPlaylist.currentItem())
-        else:
-            # Play the current item. In this case, it is not selected, but the 
+            #self.on_lswPlaylist_doubleClicked(self.lswPlaylist.currentItem())
+        #else:
+            # Play the current item. In this case, it is not selected, but the
             # state is 'stopped'
-            self.on_lswPlaylist_doubleClicked(self.lswPlaylist.currentItem())
+            #self.on_lswPlaylist_doubleClicked(self.lswPlaylist.currentItem())
 
     # Action: click on the 'previous' button.
     @QtCore.pyqtSlot()
     def on_btnPrevious_clicked(self):
         if self.playlist == []:
             return
-     
+
         # Get a reference of the current selected media.
         curIndex = self.lswPlaylist.currentRow()
-     
+
         # If the current selected media index is greater or equal to the first
         # index of the play list...
         if curIndex > 0:
@@ -180,10 +197,10 @@ class VideoPlayer(QtGui.QMainWindow):
             # If the repeat mode isn't selected then do nothing.
             if not self.repeat:
                 return
-     
+
             # otherwise, the next media to play will be the last in the list.
             index = len(self.playlist) - 1
-     
+
         # Select the previous media in the play list and play it.
         self.lswPlaylist.setCurrentRow(index, QtGui.QItemSelectionModel.SelectCurrent)
         self.on_lswPlaylist_doubleClicked(self.lswPlaylist.item(index))
@@ -198,9 +215,9 @@ class VideoPlayer(QtGui.QMainWindow):
                 del self.playlist[self.lswPlaylist.row(media)]
             except:
                 pass
-     
+
         self.updatePlayList()
-    
+
 
     # ACtion: click on the 'Repeat' button.
     @QtCore.pyqtSlot()
@@ -234,25 +251,25 @@ class VideoPlayer(QtGui.QMainWindow):
         if self.playlistTmp == []:
             # this mean the play list is not shuffled.
             self.playlistTmp = self.playlist[:]
-            
+
             # Now shuffle it.
             item = len(self.playlist) - 1
             while item > 0:
                 index = random.randint(0, item)
                 tmp = self.playlist[index]
                 self.playlist[index] = self.playlist[item]
-                self.playlist[item] = tmp        
-                item -= 1                
-            
+                self.playlist[item] = tmp
+                item -= 1
+
         else:
             # Return the list to the original state.
             self.playlist = self.playlistTmp[:]
             self.playlistTmp = []
-        
+
         # Update the play list.
-        self.updatePlayList()        
-            
-        
+        self.updatePlayList()
+
+
 
     # Action: Double an item in the play list to play it.
     @QtCore.pyqtSlot(QtCore.QModelIndex)
@@ -270,7 +287,7 @@ class VideoPlayer(QtGui.QMainWindow):
         except:
             # Here model_index is of the type QtGui.QModelIndex.
             self.vdpVideo.play(self.playlist[self.lswPlaylist.row(model_index)])
-    
+
     # Add local file
     @QtCore.pyqtSlot()
     def on_axnAddLocalFile_triggered(self):
@@ -285,11 +302,11 @@ class VideoPlayer(QtGui.QMainWindow):
     def on_axnAddURL_triggered(self):
         # Create an instance for the "Add URL" dialog.
         addURLDlg = addurl.AddURL(self)
-     
+
         # Open the dialog.
         # The program execution will stop here until user close the dialog.
         addURLDlg.exec_()
-     
+
         if addURLDlg.result() == 0:
             # Add the URLs to the play list.
             self.addMedias(addURLDlg.urls)
@@ -306,6 +323,7 @@ class VideoPlayer(QtGui.QMainWindow):
             # If the play list if currently empty, add files listed in medias
             # to the list and set play to 'True'
             if self.playlist == []:
+                print "play = True"
                 play = True
 
             # Add files to the play list.
@@ -313,17 +331,21 @@ class VideoPlayer(QtGui.QMainWindow):
             for media in medias:
                 try:
                     if isinstance(media, str):
+                        # Online file.
+                        print "Adding online video"
                         if media.startswith('http://') or media.startswith('https://') or media.startswith('mms://'):
                             self.playlist += [phonon.Phonon.MediaSource(QtCore.QUrl(media))]
                     else:
+                        # Local files
+                        # This case is simpler.
                         self.playlist += [phonon.Phonon.MediaSource(media)]
-                        print "Adding off-line video"
+                        print "Adding offline video"
                 except:
                     """
                     This version: ignore error here.
                     """
-                    print "An error has occurred" 
-                                    
+                    print "An error has occurred"
+
             # update the play list.
             self.updatePlayList()
 
@@ -354,14 +376,14 @@ class VideoPlayer(QtGui.QMainWindow):
 
         # add the new play list.
         self.lswPlaylist.addItems(playlist)
-        
-    
+
+
     # Response to timer event.
     @QtCore.pyqtSlot()
     def on_tmrTimer_timeout(self):
         # Update the playing time and the label.
         currentTime = self.vdpVideo.currentTime()
-        
+
         if currentTime == 0:
             # This is the first time the label is changed.
             self.lblTime.setText('')
@@ -371,33 +393,33 @@ class VideoPlayer(QtGui.QMainWindow):
             # If the total playing time is less than 1 hour, just show minutes
             # and seconds.
             tFormat = 'mm:ss' if totalTime < 3600000 else 'hh:mm:ss'
-     
+
             # We use Qtime for time conversions.
             currentTimeH = QtCore.QTime()
-     
+
             # Convert times to a human readable strings.
             ct = currentTimeH.addMSecs(currentTime).toString(tFormat)
-     
+
             totalTimeH = QtCore.QTime()
             tt = totalTimeH.addMSecs(totalTime).toString(tFormat)
-     
+
             # Set time to label.
             self.lblTime.setText(ct + '/' + tt)
-        
+
         # Now update the mouse status.
         # The window is in full-screen mode...
         if self.isFullScreen():
             # Update the current mouse time and position.
             mousePos = QtGui.QCursor.pos()
             mouseT = QtCore.QTime.currentTime()
-         
+
             # Normally, when the program is in full-screen mode, the mouse must
             # be hidden until user move it.
             if (mousePos != self.mousePos0 and \
                 self.cursor().shape() == QtCore.Qt.BlankCursor) or \
                 self.wdgVideoControls.isVisible():
                 self.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
-         
+
                 # Reset the time count for calculating the mouse moving time.
                 self.mouseT0 = QtCore.QTime.currentTime()
             # If user stops moving the mouse, it must stay visible at least some
@@ -405,14 +427,14 @@ class VideoPlayer(QtGui.QMainWindow):
             elif self.cursor().shape() == QtCore.Qt.ArrowCursor and \
                     self.mouseT0.secsTo(mouseT) > 1:
                 self.setCursor(QtGui.QCursor(QtCore.Qt.BlankCursor))
-         
+
             # Update the current mouse position.
             self.mousePos0 = mousePos
-         
+
             # Convert the global mouse position in the screen to the window
             # local coordinates. And get the coordinate for Y axis.
             mouseY = self.mapFromGlobal(mousePos).y()
-         
+
             # If the mouse approaches to the position in which must be the
             # controls bar, it must be visible.
             if mouseY < self.height() and \
@@ -427,24 +449,24 @@ class VideoPlayer(QtGui.QMainWindow):
             # If the mouse cursor is hidden, show it.
             if self.cursor().shape() == QtCore.Qt.BlankCursor:
                 self.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
-         
+
             # Show play list.
             if self.wdgVideoControls.isHidden():
                 self.wdgVideoControls.show()
-        
-        
+
+
     # mouseDoubleClickEvent is a protected method which called when user
     # double clicks in the GUI and this event isn't caught by any other widget.
     def mouseDoubleClickEvent(self, event):
         # Always, before process the event, we must send a copy of it to the
         # ancestor class.
         QtGui.QMainWindow.mouseDoubleClickEvent(self, event)
-     
+
         # Go to full-screen mode or exit from it.
         self.on_btnFullscreen_clicked()
-        
-        
-            
+
+
+
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     app.setApplicationName('Simple player')
