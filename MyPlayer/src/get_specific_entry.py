c2e650641a4256e96a8face8eea64c4171cf2845
Jesse Zhang <d@users.noreply.github.com>
2012-10-30 18:07:23 +0000

    Search: return html: done Get Directlink: done

--- MyPlayer/src/get_specific_entry.py
+++ MyPlayer/src/get_specific_entry.py
@@ -2,12 +2,16 @@
 
 import gdata.youtube.service
 
+# Return the entry if no error occurs, return 'None' otherwise.
 def GetEntry(video_id):
     # Initialize the service.
     yt_service = gdata.youtube.service.YouTubeService()
     
     # Get the entry.
-    entry = yt_service.GetYouTubeVideoEntry(video_id = video_id)
+    try:
+        entry = yt_service.GetYouTubeVideoEntry(video_id = video_id)
+    except:
+        entry = None        
     
     # Return the entry.
     return entry
