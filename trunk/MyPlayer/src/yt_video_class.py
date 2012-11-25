4c9631a530427406fdcb19b7ebe8cbf9e6361df3
huynhlv54@gmail.com <huynhlv54@gmail.com@abddc2c8-b0ae-73c4-6948-1f736a806b20>
2012-11-25 10:13:18 +0000

    Big change: Use web-browser to view video instead.

---
+++ trunk/MyPlayer/src/yt_video_class.py
@@ -0,0 +1,24 @@
+# A simple video class that hold information about  a video.
+# This will be useful when you want to create a play list or a list of videos
+# to download.
+class Video:
+    def init(self, link = None, direct_link = None, name = None):
+        self.setProperty(link, direct_link, name)
+
+    def setProperty(self, link, direct_link, name):
+        self.name = name # The name of the video
+        self.link = link # link to the video page
+        self.direct_link = direct_link # The link to the video file
+
+    # Get the name of the video.
+    def getName(self):
+        return self.name
+
+    # get the link to the Youtube page.
+    def getLink(self):
+        return link
+
+    # get the direct link to the video file.
+    def getDirectLink(self):
+        return link
+    
